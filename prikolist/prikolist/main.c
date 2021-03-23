1afbe63360e7fc13e4740a5bea059f447aa41f61
olexandrPeduraruK101 <78420095+olexandrPeduraruK101@users.noreply.github.com>
2021-03-23 08:17:05 +0200

    git commit

---
+++ prikolist/prikolist/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>//підключення б1бліотек
+#include<locale.h>
+#include<math.h>
+
+
+int main(void) {
+	int n;
+	int m;
+	int h;
+	setlocale(LC_CTYPE, "ru");//п1дключення п1дтримки кирилиці української мови//
+	printf("\nввед1ть число n=");
+	scanf("%d", &n);
+	m = n / 60;
+	h = n / 3600;
+	printf("\t m \t h \n");
+	printf("\t%d\t%d\n", m, h);
+	return 0;
+}
