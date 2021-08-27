f5b3761551ac66858b141c513a585de993a9f2f3
IgorMK101 <81213327+IgorMK101@users.noreply.github.com>
2021-08-27 05:03:42 +0300

    Update main.c

--- prikolist/prikolist/main.c
+++ prikolist/prikolist/main.c
@@ -9,8 +9,8 @@ int main(void) {
 	setlocale(LC_CTYPE, "ru");//п1дключення п1дтримки кирилиці української мови//
 	printf("\nввед1ть число n=");
 	scanf("%d", &n);
-	m =(float) n / 60;
+	m = n / 60;
 	printf("\t m \n");
-	printf("\t %.2f \n", m);
+	printf("\t %.f \n", m);
 	return 0;
 }
